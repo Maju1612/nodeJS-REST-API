468f2d92a74c8bcf6d9ee1e8e44ccb7f7ce1d620
Piotr Maj <piotr.maj1612@gmail.com>
2021-06-07 13:18:22 +0200

    add dotenv and mysql2 packages, create .env and conn.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const connection = mysql.createConnection({
+        host:'sql11.freemysqlhosting.net',
+        user:'sql11416779',
+        password:'QE9pMXfqfX',
+        database:'sql11416779'
+    })
+
 const postRoutes = require('./routes/post')
 
 app.use('/', postRoutes)
