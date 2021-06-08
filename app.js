c0a9939a2cecd3687f12e28504742322434be4c1
Piotr Maj <piotr.maj1612@gmail.com>
2021-06-08 12:57:39 +0200

    install knex

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const postRoutes = require('./routes/post')
 app.use(bodyParser.json())
 app.use('/', postRoutes)
 
-const port = 3000
+const port = process.env.PORT
 app.listen(port, () => console.log(`A NodeJS API is listening on port ${port}`))
